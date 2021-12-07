da612ae27d77c1cc990843f9e977e7e43a90ca65
Paulino Fonseca Veloso Junior <pfvjpoli@usp.br>
2021-12-07 03:18:06 -0300

    Adding login requirement to profile page

--- appsite/views.py
+++ appsite/views.py
@@ -1,12 +1,13 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #from .models import UserData
 #from .forms import UserDataForm
 
 
-class UserDetailView(generic.DetailView):
+class UserDetailView(LoginRequiredMixin, generic.DetailView):
     model = User
     template_name = 'appsite/detail.html'
     context_object_name = 'user'

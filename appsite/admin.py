5c2dc6956896b4e59703a7771c1229ae9816640b
Henrique Ken <henriqueken.mochizuki@gmail.com>
2021-12-07 21:23:34 -0300

    Implemented jobs and invites to lists

--- appsite/admin.py
+++ appsite/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 # admin.site.register(UserData)
 admin.site.register(List)
-admin.site.register(JobType)
+# admin.site.register(JobType)
 admin.site.register(Job)
 admin.site.register(Task)
 admin.site.register(Tag)
--- appsite/views.py
+++ appsite/views.py
@@ -1,9 +1,9 @@
-from .models import List, Job, JobType
+from .models import List, Job
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from appsite.forms import ListForm
+from appsite.forms import ListForm, InviteForm
 from django.shortcuts import get_object_or_404
 
 #from .models import UserData
@@ -33,7 +33,7 @@ class ListCreateView(generic.CreateView):
 
     def get_success_url(self):
         # Vincula criador e lista no banco de dados
-        job = Job(active_invite=False, list_id=self.object.id, user_id=self.request.user.id, type=JobType.objects.get(pk=1))
+        job = Job(active_invite=False, list_id=self.object.id, user_id=self.request.user.id, type=1)
         job.save()
 
         return reverse_lazy('appsite:list_detail', args=(self.object.id, ))
@@ -43,6 +43,49 @@ class ListDetailView(generic.DetailView):
     model = List
     template_name = 'appsite/list_detail.html'
     context_object_name = 'list'
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs) # context padrão
+
+        #Pegando os objetos de Job de acordo com o job_type (cargo)
+        creator = Job.objects.filter(list_id=self.kwargs['pk'], type=1)
+        admin = Job.objects.filter(list_id=self.kwargs['pk'], type=2)
+        follower = Job.objects.filter(list_id=self.kwargs['pk'], type=3)
+        guest = Job.objects.filter(list_id=self.kwargs['pk'], type=4)
+
+        #Pegando somente os IDs de usuário de cada um deles
+        creator_id = creator.values_list('user_id', flat=True)
+        admin_id = admin.values_list('user_id', flat=True)
+        follower_id = follower.values_list('user_id', flat=True)
+        guest_id = guest.values_list('user_id', flat=True)
+
+        #Pegando os objetos de User
+        try:
+            creator_user = User.objects.get(pk__in=creator_id)
+        except:
+            creator_user = None
+        try:
+            admin_user = User.objects.get(pk__in=admin_id)
+        except:
+            admin_user = None
+        try:
+            follower_user = User.objects.get(pk__in=follower_id)
+        except:
+            follower_user = None
+        try:
+            guest_user = User.objects.get(pk__in=guest_id)
+        except:
+            guest_user = None
+
+        print(creator_id)
+
+        #Montando os contextos
+        context['creator_user'] = creator_user
+        context['admin_user'] = admin_user
+        context['follower_user'] = follower_user
+        context['guest_user'] = guest_user
+
+        return context
 
 
 class ListUpdateView(generic.UpdateView):
@@ -52,3 +95,20 @@ class ListUpdateView(generic.UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('appsite:list_detail', args=(self.object.id, )) #redirecting to lists' page
+
+
+class InviteCreateView(generic.CreateView):
+    form_class = InviteForm
+    template_name = 'appsite/invite.html'
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.list = List.objects.get(pk=self.kwargs['pk'])
+        self.object.active_invite = True
+        self.object.type = 4
+        self.object.save()
+
+        return super(InviteCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('appsite:invite', args=(self.kwargs['pk'], )) #redirecting to invite page

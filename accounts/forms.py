46b02567e2f76370c78726fcce9332c76925ba4e
gvmossato <gabriel-vmf@hotmail.com>
2021-12-09 01:17:17 -0300

    add sign in forms style

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+# Label to placeholder
 class PlaceholderMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

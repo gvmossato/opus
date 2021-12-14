e4f9d3ddb485adf19b9c35f97df9efe7b5c871b2
Paulino Fonseca Veloso Junior <pfvjpoli@usp.br>
2021-12-14 07:33:37 -0300

    Password reset functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,9 @@ from django.views import generic
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.http import HttpResponse
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 from .forms import UserForm
 from appsite.models import Profile
@@ -63,3 +66,13 @@ def activate(request, uidb64, token):
         return HttpResponseRedirect( reverse_lazy('appsite:profile_detail', args=(user.id, ) ) )
     else:
         return HttpResponse('Activation link is invalid!')
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'accounts/password_reset.html'
+    email_template_name = 'accounts/password_reset_email.html'
+    subject_template_name = 'accounts/password_reset_subject.txt'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('login')
